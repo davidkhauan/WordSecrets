bd57b761365038d3a9fc15b48d20f9c3933794ca
davidkhauan <thalsmatsumira@gmail.com>
2024-03-17 18:16:40 -0300

    primeiro commit

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,14 @@
+import "./GameOver.css"
+
+const GameOver = ({ retry, score }) => {
+
+  return (
+    <div>
+        <h2>Fim de Jogo!</h2>
+        <h2>A sua pontuação foi: <span>{score}</span></h2>
+        <button onClick={retry}>Tentar novamente</button>
+    </div>
+  )
+}
+
+export default GameOver
---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Parabrisa", "Nitro", "Quilomêtro"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Dragão", "Goiaba"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Coração", "Glabela"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Hardware", "Roteador"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Python", "Java"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Macarrão", "Cuscuz"],
+    games: ["Persona", "Okami", "Starbound", "Terraria"]
+}
